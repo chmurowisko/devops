2d0b9afea3ed9f98baa4c3613b9172457bf3361c
Krystian Kucharski <krystian.kucharski@chmurowisko.pl>
2019-06-13 00:10:48 +0200

    Changed index.js

--- nodejs/index.js
+++ nodejs/index.js
@@ -8,9 +8,9 @@ var port = process.env.port || 1337;
 app.get('/', function(req, res){
     res.writeHead(200, {"Content-Type":"text/html"});
     if(process.env.STAGE == null){
-        res.end('<h2>Stage: Unknown</h2>');
+        res.end('<h2>Stage: Unknown</h2>' + ' </br>' + '<p>hello');
     } else{
-        res.end('<h2>Stage: ' + process.env.STAGE + ' </h2>');
+        res.end('<h1>Version: 1.0</h1>' + '<h2>Stage: ' + process.env.STAGE + ' </h2>' + '<h3> ' + process.env.RELEASE + '</h3>');
     }
 });
 
